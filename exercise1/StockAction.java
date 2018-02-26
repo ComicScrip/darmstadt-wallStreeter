73c8d9147646006a805eb8451ce49d28ceccbd2e
Pierre Genthon <genthon.pierre@gmail.com>
2018-02-26 14:23:23 +0100

    StockAction serializable

--- exercise1/StockAction.java
+++ exercise1/StockAction.java
@@ -1,4 +1,4 @@
-public abstract class StockAction {
+public abstract class StockAction implements java.io.Serializable {
     private Stock stock;
     private String uuid; // a unique identifier for the stock
     private StockActionStatus status;
