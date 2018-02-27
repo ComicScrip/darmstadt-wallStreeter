ca9e511ebf55607fcf1e1b3214290b3f97260d20
maxmaxmoser <maxmaxmoser@gmail.com>
2018-02-27 09:31:37 +0100

    Added a prototype of function "sell"

--- exercise1/TCPClient.java
+++ exercise1/TCPClient.java
@@ -14,35 +14,40 @@ public class TCPClient {
   static String line;
   static Socket socket;
   static BufferedReader fromServer;
-  static DataOutputStream toServer;
+  static ObjectOutputStream toServer;
   static UserInterface user = new UserInterface();
 
   public static void main(String[] args) throws Exception {
     socket = new Socket("localhost", 9999);
-    toServer = new DataOutputStream(     // Datastream FROM Server
-      socket.getOutputStream());
+    toServer = new ObjectOutputStream(socket.getOutputStream());
     fromServer = new BufferedReader(     // Datastream TO Server
       new InputStreamReader(socket.getInputStream()));
-    while (sendRequest()) {              // Send requests while connected
+
+  //  while (sendRequest()) {              // Send requests while connected
       receiveResponse();                 // Process server's answer
-    }
+  //  }
     socket.close();
     toServer.close();
     fromServer.close();
   }
 
-  private static boolean sendRequest() throws IOException {
-    boolean holdTheLine = true;          // Connection exists
-    user.output("Enter message for the Server, or end the session with . : ");
+  public void sell(float price, StockName name) throws IOException{
+    StockAction sa = new StockAsk(new Stock(name), price);
+    toServer.writeObject(sa);
+  }
+
+  /*
+  private static boolean sendRequest(String data) throws IOException {
     toServer.writeBytes((line = user.input()) + '\n');
     if (line.equals(".")) {              // Does the user want to end the session?
       holdTheLine = false;
     }
     return holdTheLine;
   }
-
+*/
   private static void receiveResponse() throws IOException {
-    user.output("Server answers: " +
-      new String(fromServer.readLine()) + '\n');
+    //user.output("Server answers: " +
+      //new String(fromServer.readLine()) + '\n');
+    ObjectInputStream response = new ObjectInputStream((OutputStream)fromServer.readLine());
   }
 }
